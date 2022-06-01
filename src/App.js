324bd4cbc46c55757c016a0fa13c706d21c5d65a
Alejandro Diaz Tamayo <aletamayo702@gmail.com>
2022-06-01 05:31:05 -0700

    Created category directory

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
+import Directory from './components/directory/directory.component';
 
 function App() {
-
-  const categories = [
-    {title: "Hats"},
-    {title: "Jackets"},
-    {title: "Sneakers"},
-    {title: "Womens"},
-    {title: "Mens"}
-  ]
-
   return (
-    <div className="categories-container">
-      {categories.map((category)=> {
-          return (
-            <div className="category-container">
-              {/*<img>*/}
-              <div className="category-body-container">
-                  <h2>{category.title}</h2>
-                  <p>Shop now</p>
-              </div>
-            </div>
-          )
-      })
-      }
-
-
-    </div>
+    <Directory />
   );
 }
 
