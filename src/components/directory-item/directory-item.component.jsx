0b5015b3c4379526a3785bafe92df41aaf44d853
Alejandro Diaz Tamayo <aletamayo702@gmail.com>
2022-06-08 04:31:00 -0700

    Final touches

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,15 +1,17 @@
 import { DirectoryItemContainer, BackgroundImage, Body, DirectoryItemTitle, DirectoryItemShopNow} from "./directory-item.styles" 
-
+import { useNavigate } from 'react-router-dom'
 
 const DirectoryItem = ({category}) => {
-    const {imageUrl, title} = category
+    const {imageUrl, title, route} = category
+    const navigate = useNavigate()
+    
+    const onNavigateHandler = () => navigate(route)
+
     return (
-            <DirectoryItemContainer>
-              <BackgroundImage style={{
-                backgroundImage: `url(${imageUrl})`
-              }}>
+            <DirectoryItemContainer onClick={onNavigateHandler}>
+              <BackgroundImage imageUrl={imageUrl}>
               </BackgroundImage>
-              <Body>
+              <Body to={`/shop/${title.toLowerCase()}`}>
                   <DirectoryItemTitle>{title}</DirectoryItemTitle>
                   <DirectoryItemShopNow>Shop now</DirectoryItemShopNow>
               </Body>
--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -1,5 +1,25 @@
 import styled from 'styled-components'
 
+export const BackgroundImage = styled.div`
+width: 100%;
+height: 100%;
+background-size: cover;
+background-position: center;
+background-image: ${({imageUrl}) => `url(${imageUrl})`}
+`
+export const Body = styled.div`
+height: 90px;
+padding: 0 25px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+border: 1px solid black;
+background-color: white;
+opacity: 0.7;
+position: absolute;
+`
+
 export const DirectoryItemContainer = styled.div`
 min-width: 30%;
 height: 240px;
@@ -13,18 +33,14 @@ overflow: hidden;
 &:hover {
   cursor: pointer;
 
-  & .background-image {
+  & ${BackgroundImage}{
     transform: scale(1.1);
     transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
   }
 
-  & .body {
+  & ${Body} {
     opacity: 0.9;
   }
-}
-
-&.large {
-  height: 380px;
 }
 
 &:first-child {
@@ -36,25 +52,9 @@ overflow: hidden;
 }
 `
 
-export const BackgroundImage = styled.div`
-width: 100%;
-height: 100%;
-background-size: cover;
-background-position: center;
-`
 
-export const Body = styled.div`
-height: 90px;
-padding: 0 25px;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-border: 1px solid black;
-background-color: white;
-opacity: 0.7;
-position: absolute;
-`
+
+
 export const DirectoryItemTitle = styled.h2`
 font-weight: bold;
 margin: 0 6px 0;
